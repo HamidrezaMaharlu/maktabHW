29e2bca30863abbad6c29399b01bf9e516ca7279
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-09-28 19:19:31 +0330

    add solution q2 to main.js

--- Maharlu.hamidreza_HW005_maktab84/main.js
+++ Maharlu.hamidreza_HW005_maktab84/main.js
@@ -7,3 +7,29 @@ let user = {
 
 };
 // user.sayHello()
+
+
+//--------------------------q2---------------------
+// return this => for chaining the method
+const stepCounter = {
+    step: 0,
+    increase() {
+        ++this.step;
+        return this;
+    },
+    decrease() {
+        this.step >= 0 ? --this.step : "the step is zero";
+        return this;
+    },
+    reset() {
+        this.step = 0;
+        return this;
+    },
+    read() {
+        alert(this.step);
+        return this;
+    },
+
+}
+
+stepCounter.increase().increase().increase().decrease().read()
