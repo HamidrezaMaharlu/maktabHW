5c2b24f5cd6481739ec3d86a95871982de36ac37
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-09-29 01:16:36 +0330

    add solution q4 to main index style

--- Maharlu.hamidreza_HW005_maktab84/main.js
+++ Maharlu.hamidreza_HW005_maktab84/main.js
@@ -35,9 +35,26 @@ const stepCounter = {
 
 // ------------------------------------q3---------------------------------------
 const myArray = [{name: "John", age: 45}, {name: "Sara", age: "20"}, {name: "Mike", age: 26}, {name: "Jonas", age: 14}]
+
 // use + maybe the value of age is string😉
 function sort(arr) {
     return arr.sort((a, b) => +a.age - +b.age);
 }
 
 console.log((sort(myArray)))
+
+// -----------------------------------------q4--------------------------------------
+//use obj destructing as input and set the default value
+function showNotification({top = 100, right = 100, html = "Hello", className = "welcome"} = {}) {
+    // select div with message class
+    const message = document.querySelector(".message");
+    //set style for position
+    message.setAttribute("style", `top:${top}px ;right:${right}px`);
+    //add class
+    message.classList.add(`${className}`)
+    // create paragraph and add text then append it to message
+    const p = document.createElement("p");
+    p.innerHTML = `${html}`;
+    message.append(p);
+}
+// showNotification()
