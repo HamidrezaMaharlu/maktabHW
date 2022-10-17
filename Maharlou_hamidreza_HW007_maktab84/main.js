729e5224469ea7949f010b8618aac99612a1bfa4
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-10-18 02:42:48 +0330

    solve q1-2 make func to concat and sort array

--- Maharlou_hamidreza_HW007_maktab84/main.js
+++ Maharlou_hamidreza_HW007_maktab84/main.js
@@ -17,4 +17,14 @@ function topSalary(obj) {
     console.log(...result)
 }
 
-topSalary(salaries)
+// topSalary(salaries)
+
+//--------------------------q1-2------------------------------------------
+function sort(...args) {
+    const concat=[]
+    args.forEach(item => concat.push(...item));
+    concat.sort((a, b) => b - a);
+    console.log(...concat)
+}
+sort ( [5,6,2] , [3,7,1] , [2,4,8] )
+
