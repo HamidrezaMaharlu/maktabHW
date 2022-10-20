470ad54fe963478aed2dc77ba988f38fd3450770
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-10-20 17:57:26 +0330

    make func matchPassword better

--- Maharlou_hamidreza_HW007_maktab84/main.js
+++ Maharlou_hamidreza_HW007_maktab84/main.js
@@ -78,10 +78,13 @@ function matchPassword() {
     const pw1 = document.getElementById("password");
     const pw2 = document.getElementById("re-password");
     const match = document.getElementById("match")
+    const btn = document.getElementById("registerBtn")
     if (pw1.value !== pw2.value) {
-        match.innerText="❌"
+        match.innerText = "❌"
+        btn.disabled = true
     } else {
-        match.innerText="✔"
+        match.innerText = "✔"
+        btn.disabled = false
     }
 }
 
