c8587628348628cb3e4c3402b62b58185f73c231
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-11-01 16:09:15 +0330

    solve Q2 create a func that get an elem and callback and add it to DOM

--- Maharlou_hamidreza_HW008_maktab84/main.js
+++ Maharlou_hamidreza_HW008_maktab84/main.js
@@ -13,4 +13,23 @@ const ageSetter = generator("age").bind(user)
 nameSetter("jack");
 console.log(ageSetter(21));
 
+//--------------------------------q2----------------------------
+// create func that get elem and callback and make it
+function elemCreator(elem,func) {
+    const body = document.querySelector("body")
+    //create the element
+    const createElem = document.createElement(`${elem}`);
+    //add text to it
+    createElem.innerText="salam"
+    //append it
+    body.append(createElem)
+    //call callback and pass the elem to it
+    func(createElem)
+}
+function changeBackgroundColorToRed(elem) {
+    elem.style.background="red"
+}
+elemCreator("p",changeBackgroundColorToRed)
+elemCreator("span",changeBackgroundColorToRed)
+
 
